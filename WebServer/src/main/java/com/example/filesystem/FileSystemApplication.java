e33e7783ff6287d7b4d2bd7562d5bf03a3467107
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-11-27 13:05:56 +0800

    Add files via upload

---
+++ WebServer/src/main/java/com/example/filesystem/FileSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.filesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileSystemApplication.class, args);
+    }
+
+}

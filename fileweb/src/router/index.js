85ff8a4b572e9c7f2d312582961387d0d8a0d1a7
chunyingli <txt200212@outlook.com>
2023-11-29 17:04:23 +0800

    index.js

--- fileweb/src/router/index.js
+++ fileweb/src/router/index.js
@@ -22,7 +22,7 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
+    // route level code-splitting34
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
